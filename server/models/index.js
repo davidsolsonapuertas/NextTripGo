1dad731ffa1dfa9565c2350f8815687d4013b843
David Solsona <davidsolsonapuertas@gmail.com>
2021-03-03 11:56:12 +0000

    fix(server): docker db fix

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/solo", { useNewUrlParser: true });
+mongoose.connect("mongodb://mongo:27017/solo", { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:")); // eslint-disable-line
